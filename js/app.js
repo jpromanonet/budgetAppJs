7f24f026b220e10c4e72ddf04e88e59bd9be0e19
jpromanonet <programming@jpromano.net>
2020-08-23 14:56:26 -0300

    Show balance methods

--- js/app.js
+++ js/app.js
@@ -44,6 +44,9 @@ class UI {
     } else if (total > 0) {
       this.balance.classList.remove("showRed", "showBlack");
       this.balance.classList.add("showGreen");
+    } else if (total === 0) {
+      this.balance.classList.remove("showGreen", "showRed");
+      this.balance.classList.add("showBlack");
     }
   }
 
